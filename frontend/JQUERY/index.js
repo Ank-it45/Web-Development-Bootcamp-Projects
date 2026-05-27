bf4e8facb8305a760a645413e5c2460707c60448
Ankit Chaudhary <ankitchaudhary104507@gmail.com>
2026-05-27 22:26:34 +0530

    Add Jquery project

---
+++ frontend/JQUERY/index.js
@@ -0,0 +1,18 @@
+//$("h1").addClass("big another");
+//$("h1").text("bye");
+//$("h1").html("<em>bye</em>");
+//$("h1").css("color", "red");
+// $("h1").css("color"); //it should be written in console it gives the color applied on current element
+// $("a").attr("href","https://www.reddit.com")
+
+// $("button").click(function(){ 
+//     $("h1").css("color","purple");   
+// });
+
+// $(document).keydown(function (e) { 
+//     $("h1").text(e.key);
+// });
+
+// $("h1").on("mouseover", function () {
+//     $("h1").css("color","purple");
+// });
