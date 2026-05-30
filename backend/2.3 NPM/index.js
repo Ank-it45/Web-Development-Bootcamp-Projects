fe8d6ee0ae15b0abc1abce160f27291c2a4329fc
Ankit Chaudhary <ankitchaudhary104507@gmail.com>
2026-05-30 20:56:34 +0530

    Use NPM

---
+++ backend/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// let generateName=require("sillyname");
+// let sillyyName=generateName();
+
+// console.log(`my name is ${sillyyName}.`);
+
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+console.log(`my name is ${name}.`);
